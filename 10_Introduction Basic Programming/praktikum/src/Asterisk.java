70a9855d87679d08c13807fd7f98ee7ea6d2689e
ocky24 <ockyprincella09@gmail.com>
2022-03-10 14:53:04 +0700

    kumpul screenshot praktikum 10

--- 10_Introduction Basic Programming/praktikum/src/Asterisk.java
+++ 10_Introduction Basic Programming/praktikum/src/Asterisk.java
@@ -1,15 +1,16 @@
-public class Asterisk {
-
-    private static int ast(int n) {
-        
-        for (i = )
-
-
-    }
-
-    public static void main(String[] args) {
-        int input = ast(5);
-    }
-
-
-}
+//// belum
+//public class Asterisk {
+//
+//    private static int ast(int n) {
+//
+//        for (i = )
+//
+//
+//    }
+//
+//    public static void main(String[] args) {
+//        int input = ast(5);
+//    }
+//
+//
+//}
--- 10_Introduction Basic Programming/praktikum/src/Ubahhuruf.java
+++ 10_Introduction Basic Programming/praktikum/src/Ubahhuruf.java
@@ -1,26 +1,26 @@
-// belum jadi
-public class Ubahhuruf {
-    public static String huruf(String hurufawal) {
-        char[] alfabet = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-        char[] abjad = hurufawal.toCharArray();
-        int i;
-        int x = abjad.length;
-        int c;
-        char[] hurufbaru = "";
-        for (i = 0; i<=x; i++ ); {
-            for (c = 0; c<=25;c++); {
-                if (abjad[i] == alfabet[c]) {
-                    hurufbaru[i] = alfabet[c+10];
-                }
-
-            }
-        }
-
-
-    }
-
-    public static void main(String[] args) {
-        System.out.println(huruf("SEPULSA OKE"));
-    }
-
-}
+//// belum jadi
+//public class Ubahhuruf {
+//    public static String huruf(String hurufawal) {
+//        char[] alfabet = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+//        char[] abjad = hurufawal.toCharArray();
+//        int i;
+//        int x = abjad.length;
+//        int c;
+//        char[] hurufbaru = "";
+//        for (i = 0; i<=x; i++ ); {
+//            for (c = 0; c<=25;c++); {
+//                if (abjad[i] == alfabet[c]) {
+//                    hurufbaru[i] = alfabet[c+10];
+//                }
+//
+//            }
+//        }
+//
+//
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(huruf("SEPULSA OKE"));
+//    }
+//
+//}
